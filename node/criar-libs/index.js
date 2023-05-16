0b023c429bcae6cf6e512dd87624348ee850ab41
Samuel <102474610+SamuelArantes@users.noreply.github.com>
2023-05-16 20:47:08 -0300

    atualizacao

--- node/criar-libs/index.js
+++ node/criar-libs/index.js
@@ -1,3 +1,26 @@
+import fs from "fs";
 import chalk from "chalk";
 
-console.log(chalk.red('Hello world!'));
+function trataErro(erro) {
+    throw new Error(chalk.red(erro.code, 'Não há arquivo no diretório'));
+}
+
+function pegaArquivo(caminhoDoArquivo) {
+    const encoding = 'utf-8';
+    fs.promises
+        .readFile(caminhoDoArquivo, encoding)
+        .then((texto) => console.log(chalk.green(texto)))
+        .catch(trataErro)
+}
+
+// function pegaArquivo (caminhoDoArquivo){
+//     const encoding = 'utf-8';
+//     fs.readFile(caminhoDoArquivo, encoding, (erro, texto) => {
+//         if (erro) {
+//             trataErro(erro);
+//         }
+//         console.log(chalk.green(texto));
+//     })
+// }
+
+pegaArquivo('./arquivos/texto.md')
